8353a7f6f5ad5d825e9deb50a89a41e0f304f1b2
ivan <piyifan@cctechhk.com>
2018-11-15 18:58:39 +0800

    通知被@的人，待完善

---
+++ app/Notifications/TopicReplyMention.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Notifications;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use App\Models\Reply;
+// 评论提及 @ 到的人通知
+class TopicReplyMention extends Notification
+{
+    use Queueable;
+    public $reply;
+    public function __construct(Reply $reply)
+    {
+        // 注入回复实体，方便 toDatabase 方法中的使用
+        $this->reply = $reply;
+    }
+    public function via($notifiable)
+    {
+        // 开启通知的频道
+        return ['database'];
+    }
+    public function toDatabase($notifiable)
+    {
+        $topic = $this->reply->topic;
+        $link =  $topic->link(['#reply' . $this->reply->id]);
+        // 存入数据库里的数据 给 data 字段
+        return [
+            'type' => 'mention',
+            'reply_id' => $this->reply->id,
+            'reply_content' => $this->reply->content,
+            'user_id' => $this->reply->user->id,
+            'user_name' => $this->reply->user->name,
+            'user_avatar' => $this->reply->user->avatar,
+            'topic_link' => $link,
+            'topic_id' => $topic->id,
+            'topic_title' => $topic->title,
+        ];
+    }
+}
